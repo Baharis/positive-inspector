cde5043669a8f831b4d800b35ed3254df8930cde
Daniel Tchoń <dantch@onet.eu>
2022-07-08 16:03:20 +0200

    Implemented `moment` and `kurtosis` based on analogous scipy functions (unavailable on linux)

--- positiveinspector.py
+++ positiveinspector.py
@@ -12,6 +12,7 @@ from typing import Iterable, Union
 from unittest.mock import Mock
 import numpy as np
 
+
 try:
     import olex
     from olexFunctions import OV
@@ -128,6 +129,52 @@ def hstack_strings(*strings: str) -> str:
 
 def interpret_string_booleans(s: str) -> Union[str, bool]:
     return True if s.lower() == 'true' else False if s.lower() == 'false' else s
+
+
+def moment(array: np.ndarray, moment_: int, axis: int) \
+        -> Union[float, np.ndarray]:
+    """Calculate `moment`-th moment of `array` along `axis`. Source: scipy."""
+    if moment_ == 0 or moment_ == 1:
+        s = list(array.shape)
+        del s[axis]
+        d = array.dtype.type if array.dtype.kind in 'fc' else np.float64
+        if len(s) == 0:
+            return d(1.0 if moment_ == 0 else 0.0)
+        else:
+            return np.ones(s, dtype=d) if moment_ == 0 else np.zeros(s, dtype=d)
+    else:
+        n_list = [moment_]
+        current_n = moment_
+        while current_n > 2:
+            if current_n % 2:
+                current_n = (current_n - 1) / 2
+            else:
+                current_n /= 2
+            n_list.append(current_n)
+
+        a_zero_mean = array - array.mean(axis, keepdims=True)
+        if n_list[-1] == 1:
+            s = a_zero_mean.copy()
+        else:
+            s = a_zero_mean**2
+
+        for n in n_list[-2::-1]:
+            s = s**2
+            if n % 2:
+                s *= a_zero_mean
+        return np.mean(s, axis)
+
+
+def kurtosis(array: np.ndarray, axis: int = 0) -> Union[float, np.ndarray]:
+    """Calculate kurtosis of `array` along `axis`. Source: scipy."""
+    mean = array.mean(axis=axis, keepdims=True)
+    m2 = moment(array, 2, axis)
+    m4 = moment(array, 4, axis)
+    zero = (m2 <= (np.finfo(m2.dtype).resolution * mean.squeeze(axis)) ** 2)
+    values = np.where(zero, 0, m4 / m2 ** 2.0)
+    if values.ndim == 0:
+        values = values.item()
+    return values - 3
 
 
 class MostlyDefaultDict(UserDict, abc.ABC):
@@ -472,6 +519,7 @@ class PDFGrid(object):
             ' PDF peak ypos | {posyp:13.5e} | {posyn:13.5e} | {posya:13.5e}\n' \
             ' PDF peak zpos | {poszp:13.5e} | {poszn:13.5e} | {posza:13.5e}\n' \
             '               |             x |             y |             z\n' \
+            '  PDF kurtosis | {kurpx:13.5e} | {kurpy:13.5e} | {kurpz:13.5e}\n' \
             ' map limit min | {lim0x:13.5e} | {lim0y:13.5e} | {lim0z:13.5e}\n' \
             ' map limit max | {lim1x:13.5e} | {lim1y:13.5e} | {lim1z:13.5e}'
         posp = self.positive_peak_position
@@ -493,6 +541,9 @@ class PDFGrid(object):
             posxa=posa[0],
             posya=posa[1],
             posza=posa[2],
+            kurpx=kurtosis(array=self.array.mean(axis=(1, 2))),
+            kurpy=kurtosis(array=self.array.mean(axis=(2, 0))),
+            kurpz=kurtosis(array=self.array.mean(axis=(0, 1))),
             lim0x=self.x_lims[0],
             lim1x=self.x_lims[1],
             lim0y=self.y_lims[0],
