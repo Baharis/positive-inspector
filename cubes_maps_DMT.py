a4372557c4015a7b1407e38f979ac67c8f73ace7
Daniel Tchoń <dantch@onet.eu>
2022-07-16 10:58:13 +0200

    `cubes_maps_DMT`: types are now cute <3

--- cubes_maps_DMT.py
+++ cubes_maps_DMT.py
@@ -17,13 +17,16 @@ import olex_xgrid
 import NoSpherA2
 import Wfn_Job
 
-types = [ "Q", "H",                                                                                                                                                 "He",
-         "Li","Be",                                                                                                                         "B", "C", "N", "O", "F","Ne",
-         "Na","Mg",                                                                                                                        "Al","Si", "P", "S","Cl","Ar",
-          "K","Ca",                                                                      "Sc","Ti", "V","Cr","Mn","Fe","Co","Ni","Cu","Zn","Ga","Ge","As","Se","Br","Kr",
-         "Rb","Sr",                                                                       "Y","Zr","Nb","Mo","Tc","Ru","Rh","Pd","Ag","Cd","In","Sn","Sb","Te", "I","Xe",
-         "Cs","Ba","La","Ce","Pr","Nd","Pm","Sm","Eu","Gd","Tb","Dy","Ho","Er","Tm","Yb","Lu","Hf","Ta", "W","Re","Os","Ir","Pt","Au","Hg","Tl","Pb","Bi","Po","At","Rn",
-         "Fr","Ra","Ac","Th","Pa", "U","Np","Pu","Am","Cm","Bk","Cf","Es","Fm","Md","No","Lr"]
+
+types = """
+Q  H                                                                                         He
+Li Be                                                                         B  C  N  O  F  Ne
+Na Mg                                                                         Al Si P  S  Cl Ar
+K  Ca                                           Sc Ti  V Cr Mn Fe Co Ni Cu Zn Ga Ge As Se Br Kr
+Rb Sr                                           Y  Zr Nb Mo Tc Ru Rh Pd Ag Cd In Sn Sb Te I  Xe
+Cs Ba La Ce Pr Nd Pm Sm Eu Gd Tb Dy Ho Er Tm Yb Lu Hf Ta  W Re Os Ir Pt Au Hg Tl Pb Bi Po At Rn
+Fr Ra Ac Th Pa U  Np Pu Am Cm Bk Cf Es Fm Md No Lr""".split()
+
 
 Cs = [[0, 0, 0],
       [0, 0, 1],
